26b5fb7ec968ece364319a1012e8647a8947154d
Cam C <ckc.cameron@gmail.com>
2020-11-06 22:47:43 -0800

    Write facade spec for MapFacade
    * test that facade can return latitude/longitude for given location

---
+++ spec/facades/map_facade_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe MapFacade do
+  before :each do
+    response = File.read("spec/fixtures/denver_mapquest_geocode.json")
+    stub_request(:get, "http://www.mapquestapi.com/geocoding/v1/address?key=#{ENV["MAPQUEST_KEY"]}&location=denver,%20co")
+      .to_return(status: 200, body: response)
+  end
+  it "returns latitude and longitude coordinates for a given location" do
+    location = "denver, co"
+    lat_long = MapFacade.lat_long(location)
+
+    expect(lat_long).to be_a(Hash)
+    expect(lat_long).to have_key(:lat)
+    expect(lat_long[:lat]).to be_a(Float)
+    expect(lat_long).to have_key(:lng)
+    expect(lat_long[:lng]).to be_a(Float)
+  end
+end
