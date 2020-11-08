f80e3d3b92c9b6e22adb78e8adf7ba826c0b3dda
Cam C <ckc.cameron@gmail.com>
2020-11-07 21:09:31 -0800

    Build HourlyWeather PORO

---
+++ app/poros/hourly_weather.rb
@@ -0,0 +1,35 @@
+class HourlyWeather
+  attr_reader :time,
+              :wind_speed,
+              :wind_direction,
+              :conditions,
+              :icon
+
+  def initialize(data)
+    @time = format_time(data[:time])
+    @wind_speed = format_wind_speed(data[:wind_speed])
+    @wind_direction = format_wind_direction(data[:wind_direction])
+    @conditions = data[:conditions]
+    @icon = data[:icon]
+  end
+
+  def format_time(time)
+    Time.at(time).strftime('%I:%M')
+  end
+
+  def format_wind_speed(speed)
+    "#{speed.round(0)} miles per hour"
+  end
+
+  def format_wind_direction(wind_deg)
+    if wind_deg.between?(45, 135)
+      "E"
+    elsif wind_deg.between?(135, 225)
+      "S"
+    elsif wind_deg.between?(225, 315)
+      "W"
+    else
+      "N"
+    end
+  end
+end
