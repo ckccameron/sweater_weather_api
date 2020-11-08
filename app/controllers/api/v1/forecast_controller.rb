249a2859bfa235111ad387dc8a5b55d93f3aea91
Cam C <ckc.cameron@gmail.com>
2020-11-07 22:35:53 -0800

    Update ForecastController index method to pass WeatherFacade into ForecastSerializer

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    
+    location = params[:location]
+
+    render json: ForecastSerializer.new(WeatherFacade.new(location).weather_combined)
+    # if location.nil? || location.empty?
+    #   render json: ErrorSerializer.new
+    # end
   end
 end
