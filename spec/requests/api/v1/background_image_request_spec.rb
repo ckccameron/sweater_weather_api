7a07b61819abda059b6d8bbda81d34b9ec35d2a5
Cam C <ckc.cameron@gmail.com>
2020-11-08 20:13:33 -0800

    Edit expectations of background image request spec
    * better reflection of what JSON looks like

--- spec/requests/api/v1/background_image_request_spec.rb
+++ spec/requests/api/v1/background_image_request_spec.rb
@@ -14,29 +14,38 @@ describe "background image requests" do
       expect(denver_image[:data]).to be_a(Hash)
 
       expect(denver_image[:data]).to have_key(:type)
-      expect(denver_image[:data][:type]).to eq("image")
+      expect(denver_image[:data][:type]).to eq("background_image")
 
       expect(denver_image[:data]).to have_key(:attributes)
       expect(denver_image[:data][:attributes]).to be_a(Hash)
 
-      expect(denver_image[:data][:attributes]).to have_key(:image)
-      expect(denver_image[:data][:attributes][:image]).to be_a(Hash)
+      expect(denver_image[:data][:attributes]).to have_key(:location)
+      expect(denver_image[:data][:attributes]).to have_key(:image_url)
+      expect(denver_image[:data][:attributes]).to have_key(:credit)
+      expect(denver_image[:data][:attributes][:credit]).to be_a(Hash)
 
-      expect(denver_image[:data][:attributes][:image]).to have_key(:location)
-      expect(denver_image[:data][:attributes][:image]).to have_key(:image_url)
-      expect(denver_image[:data][:attributes][:image]).to have_key(:credit)
-      expect(denver_image[:data][:attributes][:image][:credit]).to be_a(Hash)
+      expect(denver_image[:data][:attributes][:location]).to be_a(String)
+      expect(denver_image[:data][:attributes][:image_url]).to be_a(String)
 
-      expect(denver_image[:data][:attributes][:image][:location]).to be_a(String)
-      expect(denver_image[:data][:attributes][:image][:image_url]).to be_a(String)
+      expect(denver_image[:data][:attributes][:credit]).to have_key(:source)
+      expect(denver_image[:data][:attributes][:credit]).to have_key(:author)
+      expect(denver_image[:data][:attributes][:credit]).to have_key(:author_portfolio)
+      expect(denver_image[:data][:attributes][:credit]).to have_key(:logo)
 
-      expect(denver_image[:data][:attributes][:image][:credit]).to have_key(:source)
-      expect(denver_image[:data][:attributes][:image][:credit]).to have_key(:author)
-      expect(denver_image[:data][:attributes][:image][:credit]).to have_key(:logo)
+      expect(denver_image[:data][:attributes][:credit][:source]).to be_a(String)
+      expect(denver_image[:data][:attributes][:credit][:author]).to be_a(String)
+      expect(denver_image[:data][:attributes][:credit][:author_portfolio]).to be_a(String)
+      expect(denver_image[:data][:attributes][:credit][:logo]).to be_a(String)
+    end
 
-      expect(denver_image[:data][:attributes][:image][:credit][:source]).to be_a(String)
-      expect(denver_image[:data][:attributes][:image][:credit][:author]).to be_a(String)
-      expect(denver_image[:data][:attributes][:image][:credit][:logo]).to be_a(String)
+    it "can render bad request error if location is not given" do
+      get "/api/v1/background?location="
+
+      bad_request = JSON.parse(response.body, symbolize_names: true)
+
+      expect(bad_request[:data][:type]).to eq("error")
+      expect(bad_request[:data][:attributes][:status_code]).to be_an(Integer)
+      expect(bad_request[:data][:attributes][:message]).to be_a(String)
     end
   end
 end
