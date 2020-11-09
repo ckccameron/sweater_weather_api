11c512836926f7f9548829225aff43d22c98edae
Cam C <ckc.cameron@gmail.com>
2020-11-09 10:53:49 -0800

    Pass in lat and long as params for TrailsFacade spec

--- spec/facades/trails_facade_spec.rb
+++ spec/facades/trails_facade_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 describe TrailsFacade do
   it "returns results for trails near given location with information about each trail" do
+    lat = 39.9388
+    long = -105.2582
     location = "denver, co"
-    trails = TrailsFacade.new(location).trails
+    trails = TrailsFacade.new(lat, long, location).trails
 
     expect(trails).to be_an(Array)
     expect(trails[0]).to be_a(Trail)
