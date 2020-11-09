263ba24473ffe75f718a663dd6c38a6931773cf6
Cam C <ckc.cameron@gmail.com>
2020-11-09 09:02:07 -0800

    Start calling facades within TrailsController to visualize what I need to render JSON for index action

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::TrailsController < ApplicationController
   def index
+    location = params[:location]
 
+    map = MapFacade.lat_long(location)
+    lat = MapFacade.lat_long(location)[:lat]
+    lon = MapFacade.lat_long(location)[:lng]
+    weather = WeatherFacade.new(location).current_weather_temp_and_summary
+    trails = TrailsFacade.trails(lat, lon)
   end
 end
