3062b44034101e738cae940e7cf4fcd8ed1c4c50
Cam C <ckc.cameron@gmail.com>
2020-11-08 19:20:42 -0800

    Build BackgroundImageFacade to create image objects

---
+++ app/facades/background_image_facade.rb
@@ -0,0 +1,20 @@
+class BackgroundImageFacade
+  def initialize(location)
+    @location = location
+  end
+
+  def image
+    data = {
+      location: @location,
+      image_url: background_image_service[:photos][0][:url],
+      author: background_image_service[:photos][0][:photographer],
+      author_portfolio: background_image_service[:photos][0][:photographer_url]
+    }
+
+    BackgroundImage.new(data)
+  end
+
+  def background_image_service
+    BackgroundImageService.new.get_image(@location)
+  end
+end
