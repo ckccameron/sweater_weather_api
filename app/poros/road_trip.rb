32608b5017ec1c7f05baf35bf8a0fe5f2ee57ea6
Cam C <ckc.cameron@gmail.com>
2020-11-10 10:50:10 -0800

    Build RoadTrip PORO

---
+++ app/poros/road_trip.rb
@@ -0,0 +1,21 @@
+class RoadTrip
+  attr_reader :id,
+              :origin,
+              :destination,
+              :travel_time,
+              :forecast_at_eta
+
+  def initialize(data)
+    @origin = data[:origin]
+    @destination = data[:destination]
+    @travel_time_unformatted = data[:travel_time_string]
+    @travel_time = format_travel_time
+    @forecast_at_eta = data[:forecast_at_eta]
+  end
+
+  def format_travel_time
+    time = @travel_time_unformatted.split(/:/)
+
+    "#{time[0]} hr, #{time[1]} min"
+  end
+end
