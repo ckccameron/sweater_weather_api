5ce6560f24a7d7a01761427dcdacc573d5d974e7
Cam C <ckc.cameron@gmail.com>
2020-11-10 10:56:25 -0800

    Add more expectations to improve coverage of spec for RoadTripFacade

--- spec/facades/road_trip_facade_spec.rb
+++ spec/facades/road_trip_facade_spec.rb
@@ -5,7 +5,16 @@ describe RoadTripFacade do
     origin = "los angeles, ca"
     destination = "san diego, ca"
     road_trip = RoadTripFacade.new(origin, destination).road_trip
-    binding.pry
+
     expect(road_trip).to be_a(RoadTrip)
+    expect(road_trip.origin).to eq(origin)
+    expect(road_trip.origin).to be_a(String)
+    expect(road_trip.destination).to eq(destination)
+    expect(road_trip.destination).to be_a(String)
+    expect(road_trip.travel_time).to be_a(String)
+    expect(road_trip.travel_time).to_not include(":")
+    expect(road_trip.forecast_at_eta).to be_a(RoadTripWeatherSummary)
+    expect(road_trip.forecast_at_eta.temperature).to be_a(Float)
+    expect(road_trip.forecast_at_eta.conditions).to be_a(String)
   end
 end
