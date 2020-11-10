37a13577dfd8842c10c5a1d51ed2b43d3df5f53b
Cam C <ckc.cameron@gmail.com>
2020-11-09 20:32:06 -0800

    Fix set_api_key model method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,13 @@ describe User, type: :model do
     it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:password) }
   end
+
+  describe "methods" do
+    it "creates with a unique api key" do
+      user = User.create!(email: "boom@shakalaka.com", password: "123password", password_confirmation: "123password")
+
+      expect(user.api_key.nil?).to eq(false)
+      expect(user.api_key).to be_a(String)
+    end
+  end
 end
