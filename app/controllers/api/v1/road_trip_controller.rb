d2000177b9d669b3f674e8b84ea541512e2964d1
Cam C <ckc.cameron@gmail.com>
2020-11-10 21:19:43 -0800

    Add logic to create to render road trip response or error response depending on params sent with post request

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,5 +1,23 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    
+    origin = road_trip_params[:origin]
+    destination = road_trip_params[:destination]
+    api_key = road_trip_params[:api_key]
+    error_message1 = "Bad Request - Origin and Destination must be given"
+    error_message2 = "Unauthorized"
+
+    if (origin.nil? || origin.empty? ||destination.nil? || destination.empty?)
+      render json: ErrorSerializer.new(Error.new(error_message1)), status: 400
+    elsif (api_key.nil? || api_key.empty?)
+      render json: ErrorSerializer.new(Error.new(error_message2)), status: 401
+    else
+      render json: RoadTripSerializer.new(RoadTripFacade.new(origin, destination).road_trip), status: 200
+    end
+  end
+
+  private
+
+  def road_trip_params
+    params.permit(:origin, :destination, :api_key)
   end
 end
