ceaf38987ff1f6c9ddb6681376f0273fd958136b
Cam C <ckc.cameron@gmail.com>
2020-11-10 22:44:50 -0800

    Add webmock stub request to WeatherService spec

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -14,6 +14,10 @@ describe WeatherService do
   end
 
   it "returns weather forecast data when given latitude and longitude for a location" do
+    response = File.read("spec/fixtures/denver_open_weather_forecast.json")
+    stub_request(:get, "https://api.openweathermap.org/data/2.5/onecall?appid=#{ENV["WEATHER_KEY"]}&exclude=minutely&lat=39.738453&lon=-104.984853&units=imperial")
+      .to_return(status: 200, body: response)
+
     lat = 39.738453
     lon = -104.984853
     weather = @service.get_weather(lat, lon)
